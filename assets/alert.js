588d4f4e1e121cabcb42d1b22cf02960b0ac9e8c
alatperagaipa <109049283+alatperagaipa@users.noreply.github.com>
2022-07-11 08:12:51 +0700

    Add files via upload

---
+++ assets/alert.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("alert-download")
+  .addEventListener("click", function () {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: "success",
+      title: "Download Marker Berhasil",
+    });
+  });
